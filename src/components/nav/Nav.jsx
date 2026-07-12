922c5102e3dfd3e53d8726c71803d9d9cacad19a
Tapialj <ljptapia@gmail.com>
2026-07-12 17:02:45 -0500

    add logout and register/login to nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,37 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+
+import axios from "/api/axios";
+import { useAuth } from "/context/AuthProvider";
+
+import Button from "/components/Button";
 
 
 const Nav = () => {
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onClick = (link) => {
+    navigate(link);
+  };
+
+  const logout = async (e) => {
+    e.preventDefault();
+    
+    await axios.get(
+      "api/logout",
+      {
+        headers: {
+          Authorization: `Bearer ${auth?.token}`,
+          "Content-type": "application/json",
+        },
+        withCredentials: true,
+      },
+    );
+    setAuth({});
+    navigate(location.pathname);
+  };
+
   return (
     <header className="nav flex">
       <h1>AIM Movie DB</h1>
@@ -15,6 +45,19 @@ const Nav = () => {
         <span>{" | "}</span>
         <NavLink to="/actors">Actors</NavLink>
       </nav>
+      <section>
+        {
+          auth?.user ?
+            <span className="logout">
+              <h2>{auth.user.username}</h2>
+              <Button title="Logout" onClick={logout} />
+            </span> :
+            <span className="login-register">
+              <Button title="Login" onClick={() => onClick("/login")} />
+              <Button title="Register" onClick={() => onClick("/register")} />
+            </span>
+        }
+      </section>
     </header>
   );
 };
