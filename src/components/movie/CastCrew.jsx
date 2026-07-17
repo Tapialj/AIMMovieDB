f4af9441976c75f91ae81148489c07dcf597270d
Tapialj <ljptapia@gmail.com>
2026-07-16 21:37:47 -0500

    add logic for multiple directors

--- src/components/movie/CastCrew.jsx
+++ src/components/movie/CastCrew.jsx
@@ -3,26 +3,36 @@ import { Link } from "react-router-dom";
 import ListItem from "/components/ListItem";
 
 
-const CastCrew = ({ movie }) => {
+const CastCrew = ({ actors, directors }) => {
   return (
     <div className="cast-crew grid">
-      <div className="director">
+      <div className="directors">
         <h2>Directed By:</h2>
         
-        <Link to={`/director-details/${movie.director.id}`}>
-          <ListItem>
-            {movie.director.firstName}  {movie.director.lastName}
-          </ListItem>
-        </Link>
+        <div className="flex">
+          {
+            directors.map((director) => {
+              return (
+                <div key={director.id} className="director">
+                  <Link to={`/directors/${director.id}`}>
+                    <ListItem>
+                      {director.firstName}  {director.lastName}
+                    </ListItem>
+                  </Link>
+                </div>
+              );
+            })
+          }
+        </div>
       </div>
       <aside className="cast">
         <h2>Actors:</h2>
         <div className="actors grid">
           {
-            movie.actors.map((actor) => {
+            actors.map((actor) => {
               return (
                 <div key={actor.id} className="actor">
-                  <Link to={`/actor-details/${actor.id}`}>
+                  <Link to={`/actors/${actor.id}`}>
                     <ListItem>
                       {actor.firstName} {actor.lastName}
                     </ListItem>
